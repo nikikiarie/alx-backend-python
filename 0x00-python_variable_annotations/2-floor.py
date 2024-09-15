ecea790698d2d41aa94ed4236bb596c26b45eb6c
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-15 18:09:36 +0300

    to sting

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Calculates the floor of a number."""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns floor of a float."""
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""String representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """Returns a string representation of a float
+    """
+    return str(n)
